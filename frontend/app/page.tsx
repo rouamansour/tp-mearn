2d14b0ad0a8e82243cf91d537179ec3a0cf6b5a8
roua.mansour <roua.mansour@proxym-it.com>
2026-03-28 11:18:11 +0100

    feat: implement login form with email and password inputs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,66 +1,95 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [first, setFirst] = useState(true);
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState("");
+
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = ({target}: any) => {
+    const { name, value } = target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const login = () => {
+    console.log(data);
+    setData({
+      email: "",
+      password: "",
+    });
+  };
+
+  // test state w effect : boundsing w unboundsing 
+  /*useEffect(() => {
+    const Timer = setTimeout(() => {
+      console.log(first);
+      setFirst(!first);
+    }, 2000);
+    console.log("useEffect");
+    return () => {
+      console.log("clearTimeout");
+      clearTimeout(Timer);
+    };
+  }, [first]);*/
+
+  /////////////////////////// useState is asynchronous, so the value of first will not be updated immediately after calling setFirst. Therefore, when you log first right after calling setFirst, it will still show the old value. To see the updated value of first, you can use a useEffect hook that depends on first, or you can log the value of first in the handleFirst function after the state has been updated.
+  // state tetsn3 f RAM w management mt3ha y3mlha noyau react
+  // react dom compile les composants w y3mlha render f DOM w y3mlha diffing w reconciliation 3la DOM w y3mlha update f DOM
+
+  /*const handleFirst = (event:any) => {
+    //console.log(event);
+    //console.log(first);
+    //setFirst(!first);
+    setFirst((prevFirst) => {
+      console.log(prevFirst);
+      return !prevFirst;
+    });
+    // console.log(first);
+  }*/
+  // set mt3 state bch t5dm f background
+
+  const handleFirst = (event: any) => {
+    // Utilisez les valeurs ici
+    console.log("Text:", text);
+    console.log("Number:", number);
+    // Vider les champs
+    setText("");
+    setNumber("");
+    // setFirst((prevFirst) => !prevFirst);
+  };
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      <input
+        type="text"
+        className="px-4 py-2 border rounded mb-4"
+        placeholder="Enter email"
+        value={data.email} // to way binding : rabt les donness mta3 l input b state
+        name="email"
+        onChange={inputHandle}
+      />
+      <input
+        type="password"
+        className="px-4 py-2 border rounded mb-4"
+        placeholder="Enter password"
+        value={data.password}
+        name="password"
+        onChange={inputHandle}
+      />
+      <button
+        onClick={login}
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Login
+      </button>
     </div>
   );
 }
